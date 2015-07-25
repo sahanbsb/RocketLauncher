284b55fd2c9272150daf61368f1738fee527446c
Sahan Bulathsinhala <sahan.bsb@gmail.com>
2015-07-25 12:09:00 +0530

    Pilot class done.

--- src/rocketlauncher/Pilot.java
+++ src/rocketlauncher/Pilot.java
@@ -5,10 +5,37 @@
  */
 package rocketlauncher;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Sahan
  */
 public class Pilot {
+    static  HashMap <Pilot , String> PilotReg= new HashMap<>();
     
+   
+        
+        public String name ;
+        private int id ;
+        
+        Pilot(String name , int id ){
+        
+            this.name = name;
+            this.id = id;
+        
+        
+        }
+   
+    
+    public static void putToMap(String name,Pilot pilot){
+        
+        PilotReg.put(pilot,name);
+    
+    }
+    
+    public static void showPilots(){
+        
+        System.out.println(PilotReg);
+    }
 }
